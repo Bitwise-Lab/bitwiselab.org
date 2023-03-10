99afa41ce0dffddb61b2b742a4b891b3a2eb95e5
rafiqulshopon <rafiqul.dev@gmail.com>
2023-03-10 18:47:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import ConstructionMessage from './ConstructionMessage';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <ConstructionMessage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ConstructionMessage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ConstructionMessage.css';
+
+const ConstructionMessage = () => {
+  return (
+    <div className='construction-message'>
+      <h1>We're working hard to bring you something awesome!</h1>
+      <p>
+        Our website is currently under construction, but we can't wait to share
+        our new and improved site with you soon.
+      </p>
+    </div>
+  );
+};
+
+export default ConstructionMessage;
